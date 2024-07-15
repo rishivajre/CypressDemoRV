0326e40cdef81f8317c3d61a66b7db509fff1180
Rishikesh Vajre <138345204+rishivajre@users.noreply.github.com>
2024-07-15 19:46:13 +0530

    Add files via upload
    these Cypress automation files are uploaded from the device directly without command prompt

---
+++ cypress.config.js
@@ -0,0 +1,32 @@
+/// <reference types="Cypress"/>
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+
+  defaultCommandTimeout : 6000,
+
+  //video: true,
+  reporter: 'cypress-mochawesome-reporter',
+
+  retries: {
+    runMode: 1,
+    },
+  
+  projectId: "ymshak",    //for generating Mochawesome reports
+
+  e2e: {
+    setupNodeEvents(on, config) {   
+      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+    },
+    //where to find cypress related spec files: with the use of 'specPattern' property
+    specPattern: 'cypress/integration/examples/*.js',//this is available on global level
+    
+    env: {
+      "url": "https://rahulshettyacademy.com"
+    }
+  },
+
+});
